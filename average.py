39c0fa38f15fa8a76b2577b918d40d5a734e44f5
Stelios Miskedakis <108816350+Realstmiskedakis@users.noreply.github.com>
2022-07-15 11:00:04 +0300

    Update average.py

--- average.py
+++ average.py
@@ -4,36 +4,34 @@ Language: Python '3.10.5'
 Version: '1.0V'
 About this program:
 
-An average calculator can be used to calculate GPAs or anything else you need
-
+An average calculator can be used to calculate GPAs or anything else you need.
 
 """
 
 # Packages / Libraries
 import os
 
+# The List
+subjects_list = []
 
-# The Average Calculation Function
-def average():
+
+# The Average Function
+def average_calculator():
     while True:
         try:
             os.system('cls')
-            # It's a function that calculates the average of the user's input.
-            average_confirm = []
-            user = int(input(">> How many subjects you want?: "))
-            for i in range(user):
-                user_value = float(input(f"{i} Enter a value: "))
-                average_confirm.append(user_value)
-            print("Is it Correct?:", average_confirm)
-            user_confirm = input("Answer w/ ( Yes / No ): ").lower()
-            if user_confirm in ['yes', 'y']:
-                print(">> Your Average Results is: ", sum(average_confirm) / user)
-            else:
-                print(">> Rerun the program.")
-            break
-        except(TypeError, ValueError, InterruptedError):
+            print(">> This is a average calculator, can be used to calculate GPAs or anything else you need.\n")
+            user_subjects = int(input(">> Enter, how many subjects you want: "))
+            for subjects in range(user_subjects):
+                user_value = float(input(f">> {subjects} Enter a Value: "))
+                subjects_list.append(user_value)
+            os.system('cls')
+            print(">> Your values is:", subjects_list, "now I'll calculate this list.\n")
+            print(">> Results:", sum(subjects_list) / user_subjects)
+            return None
+        except(ValueError, TypeError, IndexError):
             print("Invalid Input, Exit.")
             break
 
 
-average()
+average_calculator()
